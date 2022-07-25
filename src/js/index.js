eabd0760beac48c5b5c68cf279b89eba9e490a7c
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-07-25 20:35:24 +0600

    Condition Function ADD

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,6 @@ const calculate = document.querySelector("#calculate");
 calculate.addEventListener("click", (e) => {
   e.preventDefault();
 
-  // Data Pick
-  let cashprice = parseInt(document.getElementById("cashprice").value);
-  let dp = parseInt(document.getElementById("dp").value);
-  let condition = parseInt(document.getElementById("condition").value);
-  let dpcond = dp + condition;
-  let percantage = parseInt(document.getElementById("purcentage").value);
-  let term = parseInt(document.getElementById("term").value);
-
   //Date pick
   var d = new Date(),
     month = new Array();
@@ -32,18 +24,25 @@ calculate.addEventListener("click", (e) => {
     current_year = d.getFullYear(),
     monthnum = d.getMonth() + 2;
 
+  //Today
   var today = day_of_month + "-" + month_name + "-" + current_year;
 
   // Installment Date
   insday = day_of_month - 1;
-
   var insdate = insday + "-" + monthnum + "-" + current_year;
+
+  // Data Pick
+  let cashprice = parseInt(document.getElementById("cashprice").value);
+  let dp = parseInt(document.getElementById("dp").value);
+  let condition = parseInt(document.getElementById("condition").value);
+  let dpcond = dp + condition;
+  let percantage = parseInt(document.getElementById("purcentage").value);
+  let term = parseInt(document.getElementById("term").value);
 
   // Show Cash Price
   document.getElementById("cashres").innerHTML = cashprice;
 
   // Show Hire Price
-
   var casbl = cashprice - dpcond;
   var casper = (casbl * percantage) / 100;
   var hire = casper * term;
@@ -74,13 +73,25 @@ calculate.addEventListener("click", (e) => {
   document.getElementById("insdate").innerHTML = insdate;
 
   //SHow 3 month installment
-  var instal3 = cashprice - dp;
-  var instalmon = instal3 / 3;
-  document.getElementById("inst3").innerHTML = instalmon.toFixed(0);
+  if (condition > 0) {
+    var instal3 = cashprice - dpcond;
+    var instalmon = instal3 / 3;
+    document.getElementById("inst3").innerHTML = instalmon.toFixed(0);
+  } else {
+    var instal3 = cashprice - dp;
+    var instalmon = instal3 / 3;
+    document.getElementById("inst3").innerHTML = instalmon.toFixed(0);
+  }
 
   //Show term installment
-  var insterm = cashbal / term;
-  document.getElementById("instterm").innerHTML = insterm.toFixed(0);
+  if (condition > 0) {
+    var instcond = parhire - dpcond;
+    var insterm = instcond / term;
+    document.getElementById("instterm").innerHTML = insterm.toFixed(0);
+  } else {
+    var insterm = cashbal / term;
+    document.getElementById("instterm").innerHTML = insterm.toFixed(0);
+  }
 
   //SHow Installment Bal
   document.getElementById("creditbal").innerHTML = cashbal;
