58fa43eded72fffd2397c25f2726df234258eb68
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-08-02 00:38:34 +0600

    dynamic footer

---
+++ src/js/footer.js
@@ -0,0 +1,37 @@
+document.getElementById("footer").innerHTML = `  <div class="container">
+<div class="row">
+  <div class="col-sm-12">
+    <div class="text-center">
+      <img src="src/img/logo.png" alt="logo" class="footlogo">
+    </div>
+  </div>
+  <div class="social mt-5">
+    <ul>
+      <li>
+        <a class="link" href="https://www.facebook.com/wapborhan">
+          <i class="fab fa-facebook"></i>
+        </a>
+      </li>
+      <li><a class="link" href="https://www.twitter.com/wapborhan">
+          <i class="fab fa-twitter"></i></a></li>
+      <li><a class="link" href="https://www.instagram.com/wapborhan">
+          <i class="fab fa-instagram"></i></a></li>
+      <li><a class="link" href="https://www.github.com/wapborhan">
+          <i class="fab fa-github"></i></a></li>
+      <li><a class="link" href="https://www.linkedin.com/in/wapborhan">
+          <i class="fab fa-linkedin"></i></a></li>
+    </ul>
+  </div>
+  <!--- END COL -->
+</div>
+<!--- END ROW -->
+<div class="row">
+  <div class="col-lg-12 col-sm-12 col-xs-12">
+    <p class="copyright">Copyright © 2022 RA Card | Developed By <a class="link"
+        href="https://www.wapborhan.com">Borhan
+        Uddin</a>.</p>
+  </div>
+  <!--- END COL -->
+</div>
+<!--- END ROW -->
+</div>`;
