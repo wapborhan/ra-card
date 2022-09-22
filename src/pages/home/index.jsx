dc8e456ee1270aa4eace0454fdb6d42e691831c5
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-23 01:51:49 +0600

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,8 +2,33 @@ import React from "react";
 
 const index = () => {
   return (
-    <div>
-      <div className="ds">Home</div>
+    <div className="main mt-5 mb-5">
+      <div className="container">
+        <div className="row m-sm-3">
+          <div className="col-md-6">
+            <a href="sale.html">
+              <div className="card rounded ">
+                <img
+                  src="src/img/sale.jpg"
+                  className="card-img-top img-flued"
+                  alt="..."
+                />
+              </div>
+            </a>
+          </div>
+          <div className="col-md-6">
+            <a href="paid.html">
+              <div className="card rounded">
+                <img
+                  src="src/img/paid.jpg"
+                  className="card-img-top"
+                  alt="..."
+                />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
